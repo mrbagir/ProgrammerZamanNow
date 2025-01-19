2d11efc247ffc08ad6545a2ec02c75cb4945cd09
GeryTrap <bagir3008@gmail.com>
2025-01-19 11:10:48 +0700

    update belajar-golang-restful-api - move router

--- Golang/belajar-golang-restful-api/main.go
+++ Golang/belajar-golang-restful-api/main.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"programmerzamannow/belajar-golang-restful-api/app"
 	"programmerzamannow/belajar-golang-restful-api/controller"
-	"programmerzamannow/belajar-golang-restful-api/exception"
 	"programmerzamannow/belajar-golang-restful-api/helper"
 	"programmerzamannow/belajar-golang-restful-api/middleware"
 	"programmerzamannow/belajar-golang-restful-api/repository"
@@ -12,7 +11,6 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -22,16 +20,7 @@ func main() {
 	categoryRepository := repository.NewCategoryRepository()
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
-
-	router := httprouter.New()
-
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
-
-	router.PanicHandler = exception.ErrorHandler
+	router := app.NewRouter(categoryController)
 
 	server := http.Server{
 		Addr:    "localhost:3000",
